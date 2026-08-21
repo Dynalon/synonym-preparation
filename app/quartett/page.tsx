3ec9e6213570939db74c3f97107828c37c463be9
Timo Dörr <timo@latecrew.de>
2026-08-21 12:04:52 +0200

    Style card browser navigation

--- app/quartett/page.tsx
+++ app/quartett/page.tsx
@@ -5,6 +5,9 @@ import carDatabase from "./data/cars.json"
 
 const NUM_CARDS = carDatabase.cards.length
 
+const buttonClass =
+  "flex items-center bg-amber-600 hover:bg-amber-400 disabled:bg-gray-500 disabled:text-gray-200 p-2 text-white"
+
 export default function QuartettDeck() {
   const [selectedCard, setSelectedCard] = useState(1)
   const car = carDatabase.cards[selectedCard - 1]
@@ -15,14 +18,14 @@ export default function QuartettDeck() {
     <div className="w-full">
       <h1 className="text-3xl mb-5 font-serif">Quartett Deck Browser</h1>
       <div className="w-full flex flex-col gap-4 items-center">
-        <div className="text-md flex justify-between items-center gap-2">
-          <button onClick={dec} className="flex items-center bg-blue-400 p-2 text-white">
+        <div className="w-[400px] text-md flex justify-between items-center gap-2">
+          <button disabled={selectedCard === 1} onClick={dec} className={buttonClass}>
             {"<"}
           </button>
           <span>
             Card {selectedCard}/{NUM_CARDS}
           </span>
-          <button onClick={inc} className="flex items-center bg-blue-400 p-2 text-white">
+          <button disabled={selectedCard === 100} onClick={inc} className={buttonClass}>
             {">"}
           </button>
         </div>
