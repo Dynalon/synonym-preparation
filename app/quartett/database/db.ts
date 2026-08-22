4b8d3e473824832e6a3aae59e2ceef3388844761
Timo Dörr <timo@latecrew.de>
2026-08-22 13:19:27 +0200

    Add db intiialization

--- app/quartett/database/db.ts
+++ app/quartett/database/db.ts
@@ -1,6 +1,6 @@
 import { Dexie, EntityTable } from "dexie"
-
 import { Car } from "@/quartett/types"
+import carDatabase from "../database/cars.json"
 
 export const db = new Dexie("Quartett") as Dexie & {
   cars: EntityTable<Car, "id">
@@ -9,3 +9,6 @@ export const db = new Dexie("Quartett") as Dexie & {
 db.version(1).stores({
   cars: "id",
 })
+
+// intialize on create from our json datastore
+db.on("populate", async () => await db.cars.bulkAdd(carDatabase.cards))
