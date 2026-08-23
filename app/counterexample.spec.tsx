398ef9b1d16c6cbe110de40d7d912a5a5aa688cc
Timo Dörr <timo@latecrew.de>
2026-08-23 18:10:05 +0200

    use page object pattern for tests

--- app/counterexample.spec.tsx
+++ app/counterexample.spec.tsx
@@ -2,35 +2,47 @@ import { render, screen, fireEvent } from "@testing-library/react"
 import { describe, expect, it } from "vitest"
 import { CounterExampleZustand, useCounterStore } from "./counterexample"
 
+const counterPO = {
+  get label() {
+    return screen.getByTestId("counter-value")
+  },
+  get incrementButton() {
+    return screen.getByTestId("counter-example-increment")
+  },
+  get decrementButton() {
+    return screen.getByTestId("counter-example-decrement")
+  },
+}
+
 describe("counter example", () => {
   it("should start with a value of zero", () => {
     render(<CounterExampleZustand />)
-    expect(screen.getByTestId("counter-value")).toHaveTextContent("counter=0")
+    expect(counterPO.label).toHaveTextContent("counter=0")
   })
 
   it("should increment when we click plus", () => {
     render(<CounterExampleZustand />)
-    const increment = () => fireEvent.click(screen.getByTestId("counter-example-increment"))
+    const increment = () => fireEvent.click(counterPO.incrementButton)
 
     increment()
-    expect(screen.getByTestId("counter-value")).toHaveTextContent("counter=1")
+    expect(counterPO.label).toHaveTextContent("counter=1")
     increment()
-    expect(screen.getByTestId("counter-value")).toHaveTextContent("counter=2")
+    expect(counterPO.label).toHaveTextContent("counter=2")
     increment()
-    expect(screen.getByTestId("counter-value")).toHaveTextContent("counter=3")
+    expect(counterPO.label).toHaveTextContent("counter=3")
   })
 
   it("should decrement when we click minus", () => {
     useCounterStore.setState((state) => ({ ...state, counter: 3 }))
     render(<CounterExampleZustand />)
-    const decrement = () => fireEvent.click(screen.getByTestId("counter-example-decrement"))
+    const decrement = () => fireEvent.click(counterPO.decrementButton)
 
-    expect(screen.getByTestId("counter-value")).toHaveTextContent("counter=3")
+    expect(counterPO.label).toHaveTextContent("counter=3")
     decrement()
-    expect(screen.getByTestId("counter-value")).toHaveTextContent("counter=2")
+    expect(counterPO.label).toHaveTextContent("counter=2")
     decrement()
-    expect(screen.getByTestId("counter-value")).toHaveTextContent("counter=1")
+    expect(counterPO.label).toHaveTextContent("counter=1")
     decrement()
-    expect(screen.getByTestId("counter-value")).toHaveTextContent("counter=0")
+    expect(counterPO.label).toHaveTextContent("counter=0")
   })
 })
